1d18be636b03b79ca1d20ea761331a6c3fd7d0b7
mir <m_dev@tuta.io>
2022-01-28 04:30:35 +0600

    adding edit and delete option

--- src/main/java/com/mir/gametics/DatabasePageController.java
+++ src/main/java/com/mir/gametics/DatabasePageController.java
@@ -10,14 +10,16 @@ import javafx.scene.control.cell.PropertyValueFactory;
 
 public class DatabasePageController {
 
-
-
     @FXML
     private TableView<Game> tableView;
     @FXML
     private TextField gameNameField;
     @FXML
     private TextField gameCategoryField;
+    @FXML
+    private Button editBtn;
+    @FXML
+    private Button deleteBtn;
 
 
     private TableColumn<Game, String> firstColumn;
@@ -28,14 +30,38 @@ public class DatabasePageController {
 
     @FXML
     public void initialize() {
+
+        initializeTable();
+
+        editBtn.setVisible(false);
+        deleteBtn.setVisible(false);
+        deleteBtn.setOnAction(event -> {
+            Game selectedGame = tableView.getSelectionModel().getSelectedItem();
+            tableView.getItems().remove(selectedGame);
+        });
+
+        tableView.getSelectionModel().selectedItemProperty().addListener(((observableValue, game, t1) -> {
+            if(t1 != null) {
+                editBtn.setVisible(true);
+                deleteBtn.setVisible(true);
+            }
+        }));
+    }
+
+    public void initializeTable() {
+        // Table Columns
         firstColumn = new TableColumn<>("Name");
         secondColumn = new TableColumn<>("Category");
 
+        // Setting value type for column
         firstColumn.setCellValueFactory(new PropertyValueFactory<>("gameName"));
         secondColumn.setCellValueFactory(new PropertyValueFactory<>("gameCategory"));
 
         gameModel = new GameModel();
+
+        // setting item on table
         tableView.setItems(gameModel.getGameList());
+        // adding column to table
         tableView.getColumns().addAll(firstColumn, secondColumn);
     }
 
@@ -45,4 +71,6 @@ public class DatabasePageController {
         String category = gameCategoryField.getText();
         gameModel.addGame(new Game(name, category));
     }
+
+
 }
